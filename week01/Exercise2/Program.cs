f90b48bd18e2220a82f1d7c23d56f6156188c9cb
mukiwamelissa <mukiwamelissa@gmail.com>
2026-05-14 22:35:06 +0200

    task complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,48 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage?");
+        string userinput = Console.ReadLine();
+        int percentage = int.Parse(userinput);
+
+        string letter = "";
+
+
+        if (percentage >= 90) 
+        
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80) 
+        
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70) 
+        
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed this term.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't make it this term. Please try again next term.");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+
+            Console.Write("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        
+
+            if (number > guess)
+            {
+                 Console.WriteLine("Higher");
+            }
+
+             else if (number < guess)
+            {
+                  Console.WriteLine("Lower");
+            }
+
+             else
+            {
+                 Console.WriteLine("You guessedit");
+            }
+        }
+    
     }
 }
