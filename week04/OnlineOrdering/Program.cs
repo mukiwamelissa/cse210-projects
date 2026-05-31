10da724bcd5f186cca82bcae0aa99ad639b24eed
mukiwamelissa <mukiwamelissa@gmail.com>
2026-05-31 23:18:28 +0200

    done all

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,183 @@
 using System;
+using System.Collections.Generic;
 
+// Address Class
+public class Address
+{
+    private string street;
+    private string city;
+    private string stateProvince;
+    private string country;
+
+    public Address(string street, string city, string stateProvince, string country)
+    {
+        this.street = street;
+        this.city = city;
+        this.stateProvince = stateProvince;
+        this.country = country;
+    }
+
+    public bool IsUSA()
+    {
+        return country.ToUpper() == "USA";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{street}\n{city}, {stateProvince}\n{country}";
+    }
+}
+
+// Customer Class
+public class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public Address GetAddress()
+    {
+        return address;
+    }
+
+    public bool LivesInUSA()
+    {
+        return address.IsUSA();
+    }
+}
+
+// Product Class
+public class Product
+{
+    private string name;
+    private string productId;
+    private double price;
+    private int quantity;
+
+    public Product(string name, string productId, double price, int quantity)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return price * quantity;
+    }
+
+    public string GetPackingInfo()
+    {
+        return $"Product: {name}, ID: {productId}";
+    }
+}
+
+// Order Class
+public class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+        products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product product in products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        total += customer.LivesInUSA() ? 5 : 35;
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+
+        foreach (Product product in products)
+        {
+            label += product.GetPackingInfo() + "\n";
+        }
+
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{customer.GetName()}\n{customer.GetAddress().GetFullAddress()}";
+    }
+}
+
+// Main Program
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Order 1 (USA Customer)
+        Address address1 = new Address(
+            "123 Main Street",
+            "Phoenix",
+            "Arizona",
+            "USA");
+
+        Customer customer1 = new Customer("John Smith", address1);
+
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Laptop", "P101", 800, 1));
+        order1.AddProduct(new Product("Mouse", "P102", 20, 2));
+        order1.AddProduct(new Product("Keyboard", "P103", 50, 1));
+
+        // Order 2 (International Customer)
+        Address address2 = new Address(
+            "45 King Road",
+            "Johannesburg",
+            "Gauteng",
+            "South Africa");
+
+        Customer customer2 = new Customer("Melissa Mukiwa", address2);
+
+        Order order2 = new Order(customer2);
+
+        order2.AddProduct(new Product("Phone", "P201", 600, 1));
+        order2.AddProduct(new Product("Charger", "P202", 25, 2));
+
+        // Display Order 1
+        Console.WriteLine("ORDER 1");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost()}");
+
+        Console.WriteLine("\n------------------------\n");
+
+        // Display Order 2
+        Console.WriteLine("ORDER 2");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost()}");
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,94 @@
 using System;
+using System.Collections.Generic;
+
+class Comment
+{
+    public string Name { get; set; }
+    public string Text { get; set; }
+
+    public Comment(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+}
+
+class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+
+    private List<Comment> comments = new List<Comment>();
+
+    public Video(string title, string author, int length)
+    {
+        Title = title;
+        Author = author;
+        Length = length;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return comments;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("C# Basics", "Nelson Williams", 600);
+        video1.AddComment(new Comment("Alice", "Great tutorial!"));
+        video1.AddComment(new Comment("Ben", "Very helpful."));
+        video1.AddComment(new Comment("Larry", "Thanks for sharing."));
+        videos.Add(video1);
+
+        Video video2 = new Video("Web Development", "Merry Richardson", 900);
+        video2.AddComment(new Comment("Junior", "Excellent explanation."));
+        video2.AddComment(new Comment("Lissa", "I learned a lot."));
+        video2.AddComment(new Comment("Mark", "Keep making videos!"));
+        videos.Add(video2);
+
+        Video video3 = new Video("Object-Oriented Programming", "Michael Daniels", 750);
+        video3.AddComment(new Comment("Andy", "Well presented."));
+        video3.AddComment(new Comment("Ella", "Very informative."));
+        video3.AddComment(new Comment("Emmanuel", "Good examples."));
+        videos.Add(video3);
+
+        Video video4 = new Video("HTML and CSS", "Christina Maritz", 500);
+        video4.AddComment(new Comment("Susan", "Easy to follow."));
+        video4.AddComment(new Comment("Jacob", "Loved this lesson."));
+        video4.AddComment(new Comment("Chris", "Thank you!"));
+        videos.Add(video4);
+
+        foreach (Video video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length: {video.Length} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetCommentCount()}");
+
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.Name}: {comment.Text}");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
+
